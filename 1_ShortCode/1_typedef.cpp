58eb4c7bf5d78f95299e7d09c83bd7105abdb1ed
Ravi <ravigautamcs@gmail.com>
2021-08-28 18:46:45 +0530

    first commit

---
+++ 1_ShortCode/1_typedef.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+
+int main(){
+
+    vi a;
+    int b=5;
+    for(int i=0; i<b; i++){
+        int x; cin>>x;
+        a.push_back(x);
+    }
+
+    for(int i=0; i<b; i++){
+        cout<<a[i]<<" ";
+    }
+
+    cout<<endl;
+    return 0;
+}
